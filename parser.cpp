2831f1452dc06c7fcf152dfc636a691621e06b7b
tfordjr <98005445+tfordjr@users.noreply.github.com>
2023-11-17 17:17:55 -0600

    switched to pass by reference to solve stale data

--- parser.cpp
+++ parser.cpp
@@ -20,72 +20,72 @@ string tokenNames[]= {"EOFTk", "IDTk", "INTTk", "ASSIGNTk",
 Scanner::Token tk;
 char c;
 
-node *Program(Scanner);
-node *Vars(Scanner);
-node *VarList(Scanner);
-node *Exp(Scanner);
-node *M(Scanner);
-node *N(Scanner);
-node *R(Scanner);
-node *Stats(Scanner);
-node *Mstat(Scanner);
-node *Stat(Scanner);
-node *Block(Scanner);
-node *In(Scanner);
-node *Out(Scanner);
-node *If(Scanner);
-node *Loop(Scanner);
-node *Assign(Scanner);
-node *RO(Scanner);
+node *Program(Scanner*);
+node *Vars(Scanner*);
+node *VarList(Scanner*);
+node *Exp(Scanner*);
+node *M(Scanner*);
+node *N(Scanner*);
+node *R(Scanner*);
+node *Stats(Scanner*);
+node *Mstat(Scanner*);
+node *Stat(Scanner*);
+node *Block(Scanner*);
+node *In(Scanner*);
+node *Out(Scanner*);
+node *If(Scanner*);
+node *Loop(Scanner*);
+node *Assign(Scanner*);
+node *RO(Scanner*);
 void errorMsg(string);
 
-node *Program(Scanner scanner){
+node *Program(Scanner *scanner){
     node *node = createNode(PROGRAMn);
     node-> child1 = Vars(scanner);
 
     if (tk.lexeme != "xopen")
         errorMsg("xopen");
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     node-> child2 = Stats(scanner);
 
     if (tk.lexeme != "xclose")
         errorMsg("xclose");
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     return node;
 }
 
-node *Vars(Scanner scanner){
+node *Vars(Scanner *scanner){
     node *node = createNode(VARSn);
     if(tk.lexeme == "xdata"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = VarList(scanner);
     } 
     return node;    
 }
 
-node *VarList(Scanner scanner){
+node *VarList(Scanner *scanner){
     node *node = createNode(VARLISTn);
 
     if (tk.type != "IDENTIFIER")
         errorMsg("IDENTIFIER");    
     node-> tk1 = tk;
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     if (tk.lexeme != ":") 
         errorMsg("COLON");
     node-> tk2 = tk;
-    tk = scanner.getNextToken(); 
+    tk = (*scanner).getNextToken(); 
     
     if (tk.type != "INTEGER")
         errorMsg("INTEGER");
     node-> tk3 = tk;
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     if (tk.lexeme == ";"){
         node-> tk4 = tk;
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
     } else if (tk.type == "IDENTIFIER") {
         node-> child1 = VarList(scanner);
     } else {
@@ -95,21 +95,21 @@ node *VarList(Scanner scanner){
     return node;
 }
 
-node *Exp(Scanner scanner){
+node *Exp(Scanner *scanner){
     node *node = createNode(EXPn);
 
     node-> child1 = M(scanner);   // Create <M> 
 
     if (tk.lexeme == "/" || tk.lexeme == "*"){
         node-> tk1 = tk;
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = Exp(scanner);
     }         
 
     return node;
 }
 
-node *M(Scanner scanner){
+node *M(Scanner *scanner){
     node *node = createNode(Mn);
 
     node-> child1 = N(scanner);
@@ -117,18 +117,18 @@ node *M(Scanner scanner){
     return node;
 }
 
-node *N(Scanner scanner){
+node *N(Scanner *scanner){
     node *node = createNode(Nn);
 
     if (tk.lexeme == "~"){   // ~ <N>
         node-> tk1 = tk;
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = N(scanner);
     } else {                // <R> - <N> || <R> case
         node-> child1 = R(scanner); // This will advance tk, we don't need to 
         if (tk.lexeme == "-"){    // <R> - <N> case
             node-> tk1 = tk;
-            tk = scanner.getNextToken();
+            tk = (*scanner).getNextToken();
             node-> child1 = N(scanner); 
         } 
     }
@@ -136,7 +136,7 @@ node *N(Scanner scanner){
     return node;
 }
 
-node *R(Scanner scanner){
+node *R(Scanner *scanner){
     node *node = createNode(Rn);
 
     if (tk.lexeme == "(") {        
@@ -148,47 +148,47 @@ node *R(Scanner scanner){
     else
         node->tk1 = tk;
 
-    tk = scanner.getNextToken();  
+    tk = (*scanner).getNextToken();  
     return node;
 }
 
-node *Stats(Scanner scanner){
+node *Stats(Scanner *scanner){
     node *node = createNode(STATSn);
     node-> child1 = Stat(scanner);
     node-> child2 = Mstat(scanner);
     return node;
 }
 
-node *Mstat(Scanner scanner){   // I think this code is good but too early to test
+node *Mstat(Scanner *scanner){   // I think this code is good but too early to test
     node *node = createNode(MSTATn);
-    if (tk.lexeme == "xin" || tk.lexeme == "xout" || tk.lexeme == "{" || 
-    tk.lexeme == "xcond" || tk.lexeme == "xloop" || tk.lexeme == "xlet"){
-        node-> child1 = Stat(scanner);
-        node-> child2 = Mstat(scanner);   
-    }
+    // if (tk.lexeme == "xin" || tk.lexeme == "xout" || tk.lexeme == "{" || 
+    // tk.lexeme == "xcond" || tk.lexeme == "xloop" || tk.lexeme == "xlet"){
+    //     node-> child1 = Stat(scanner);
+    //     node-> child2 = Mstat(scanner);   
+    // }
     return node;
 }
 
-node *Stat(Scanner scanner){
+node *Stat(Scanner *scanner){
     node *node = createNode(STATn);      
 
     if (tk.lexeme == "xin") {
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = In(scanner);
     } else if (tk.lexeme == "xout"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = Out(scanner);
     } else if (tk.lexeme == "{"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = Block(scanner);
     } else if (tk.lexeme == "xcond"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = If(scanner);
     } else if (tk.lexeme == "xloop"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = Loop(scanner);
     } else if (tk.lexeme == "xlet"){
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node-> child1 = Assign(scanner);
     } else {
         errorMsg("xin or xout or { or xcond or xloop or xlet");
@@ -197,7 +197,7 @@ node *Stat(Scanner scanner){
     return node;
 }
 
-node *Block(Scanner scanner){
+node *Block(Scanner *scanner){
     node *node = createNode(BLOCKn);
 
     node-> child1 = Vars(scanner);
@@ -205,60 +205,52 @@ node *Block(Scanner scanner){
 
     if(tk.lexeme != "}")
         errorMsg("}");
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     return node;
 }
 
-node *In(Scanner scanner){
+node *In(Scanner *scanner){
     node *node = createNode(INn);
 
     if (tk.lexeme != ">>")
         errorMsg(">>");    
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
     
     if (tk.type != "IDENTIFIER")
         errorMsg("IDENTIFIER");    
     node-> tk1 = tk;
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     if (tk.lexeme != ";")
         errorMsg(";");  
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     return node;
 }
 
-node *Out(Scanner scanner){
+node *Out(Scanner *scanner){
     node *node = createNode(OUTn);
 
     if (tk.lexeme != "<<")
         errorMsg("<<"); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
-    node-> child1 = Exp(scanner);    
-
-    cout << "\n\nFlag1: tk value: " << tk.lexeme << "\n\n";
+    node-> child1 = Exp(scanner);    // <EXP>
 
     if (tk.lexeme != ";")
         errorMsg(";");         
-    tk = scanner.getNextToken();
-
-    cout << "\n\nFlag2: tk value: " << tk.lexeme << "\n\n";
-
-    tk = scanner.getNextToken();
-
-    cout << "\n\nFlag3: tk value: " << tk.lexeme << "\n\n";
-
+    tk = (*scanner).getNextToken();
+    
     return node;
 }
 
-node *If(Scanner scanner){
+node *If(Scanner *scanner){
     node *node = createNode(IFn);
 
     if (tk.lexeme != "[")
         errorMsg("["); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     node-> child1 = Exp(scanner);
     node-> child2 = RO(scanner);
@@ -266,19 +258,19 @@ node *If(Scanner scanner){
 
     if (tk.lexeme != "]")
         errorMsg("]"); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     node-> child4 = Stat(scanner);
 
     return node;
 }
 
-node *Loop(Scanner scanner){
+node *Loop(Scanner *scanner){
     node *node = createNode(LOOPn);
 
     if (tk.lexeme != "[")
         errorMsg("["); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     node-> child1 = Exp(scanner);
     node-> child2 = RO(scanner);
@@ -286,36 +278,36 @@ node *Loop(Scanner scanner){
 
     if (tk.lexeme != "]")
         errorMsg("]"); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     node-> child4 = Stat(scanner);
 
     return node;
 }
 
-node *Assign(Scanner scanner){
+node *Assign(Scanner *scanner){
     node *node = createNode(ASSIGNn);
 
     if (tk.type != "IDENTIFIER")
         errorMsg("IDENTIFIER");    
     node-> tk1 = tk;
-    tk = scanner.getNextToken();   
+    tk = (*scanner).getNextToken();   
 
     node-> child1 = Exp(scanner);
 
     if (tk.lexeme != ";")
         errorMsg(";"); 
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     return node;
 }
 
-node *RO(Scanner scanner){
+node *RO(Scanner *scanner){
     node *node = createNode(ROn);
     
     if (tk.lexeme == "<<" || tk.lexeme == ">>"){
         node-> tk1 = tk;
-        tk = scanner.getNextToken();
+        tk = (*scanner).getNextToken();
         node->tk2 = tk;
         return node;
     } 
@@ -323,7 +315,7 @@ node *RO(Scanner scanner){
     if (tk.lexeme != "<" && tk.lexeme != ">" && tk.lexeme != "=" && tk.lexeme != "%")
         errorMsg("< or > or = or %");
     node-> tk1 = tk;
-    tk = scanner.getNextToken();
+    tk = (*scanner).getNextToken();
 
     return node;
 }
@@ -336,7 +328,7 @@ node *parser(string input) {
     tk = scanner.getNextToken();
 
     node *root;
-    root = Program(scanner);    
+    root = Program(&scanner);    
 
     if (tk.type != "EOF"){
         cout << "Error: Parsing Error\n";
